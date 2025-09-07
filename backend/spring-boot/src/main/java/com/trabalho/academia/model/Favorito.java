db0780054e0cd8639ca0031e6ec8db0f34ca1718
Felipe <felipe.in209@gmail.com>
2025-09-07 17:22:05 -0300

    Fixed the favorito mistake.

--- backend/spring-boot/src/main/java/com/trabalho/academia/model/Favorito.java
+++ backend/spring-boot/src/main/java/com/trabalho/academia/model/Favorito.java
@@ -3,15 +3,13 @@ package com.trabalho.academia.model;
 import java.sql.Date;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import lombok.Data;
@@ -19,19 +17,16 @@ import lombok.Data;
 @Data 
 @Entity
 public class Favorito {
-    //@Id
-    //@GeneratedValue(strategy=GenerationType.IDENTITY)
-    @EmbeddedId
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId("usuarioId")
-    @JoinColumn(name = "usuarioId")
+    @JoinColumn(name = "usuario")
     private Usuario usuario;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId("treinoId")
-    @JoinColumn(name = "treinoId")
+    @JoinColumn(name = "treino")
     private Treino treino;
 
     @Column(name="data")
